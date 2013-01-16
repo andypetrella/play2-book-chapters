07a435046cba76967449ff64f97ab091f9ff44cb
Andy Petrella <andy.petrella@gmail.com>
2013-01-17 00:42:40 +0100

    adaptation for the review

--- chap4/play-jbook/app/models/User.java
+++ chap4/play-jbook/app/models/User.java
@@ -11,34 +11,34 @@ import javax.persistence.*;
 
 @Entity
 public class User extends play.db.ebean.Model {
-    @Required
-    public String name;
+  @Required
+  public String name;
 
-    @Required
-    @Email
-    @Id
-    public String email;
+  @Required
+  @Email
+  @Id
+  public String email;
 
-    @Required
-    @Min(0)
-    @Max(150)
-    public Integer age;
+  @Required
+  @Min(0)
+  @Max(150)
+  public Integer age;
 
-    @Required
-    public Boolean female;
+  @Required
+  public Boolean female;
 
-    @Valid
-    @Required
-    @ManyToOne(cascade=CascadeType.ALL)
-    public Address address = new Address();
+  @Valid
+  @Required
+  @ManyToOne(cascade=CascadeType.ALL)
+  public Address address = new Address();
 
-    @Valid
-    public List<User> friends;
+  @Valid
+  public List<User> friends;
 
-    @Valid
-    public Option<User> spouse;
+  @Valid
+  public Option<User> spouse;
 
-    public static Finder<String,User> find = new Finder<String,User>(
-        String.class, User.class
-    );
+  public static Finder<String,User> find = new Finder<String,User>(
+    String.class, User.class
+  );
 }
